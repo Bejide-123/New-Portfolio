19849953fb95620423b82202b4b500ff032171e3
Bejide-123 <isbejide10@gmail.com>
2025-09-22 00:48:53 +0100

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import './index.css'
 import Home from './Components/Home'
 import About from './Components/About'
-import Skills from './Components/Skills'
 
 const App = () => {
   return (
     <div>
       <Home />
       <About />
-      <Skills />
     </div>
   )
 }
